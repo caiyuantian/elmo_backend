4dce833a63f2ffd4683f020725a8fbd569293667
caiyuantian <caiyuantian@gmail.com>
2019-08-04 22:15:40 +1000

    remove dist

--- dist/controllers/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var users_1 = require("./users");
-var tweets_1 = require("./tweets");
-module.exports = {
-    User: new users_1.User(),
-    Tweet: new tweets_1.Tweet()
-};
